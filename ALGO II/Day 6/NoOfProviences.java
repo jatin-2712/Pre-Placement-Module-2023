3d13a66e4a35cd2e91f4abf2cbb574a99f576185
jatin-2712 <75720589+jatin-2712@users.noreply.github.com>
2022-08-09 16:05:22 +0530

    Completed Day-6

---
+++ ALGO II/Day 6/NoOfProviences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        boolean[] visited= new boolean[isConnected.length];
+        Arrays.fill(visited, false);
+        int res=0;
+
+        for(int i=0; i<isConnected.length; i++) {
+            if(!visited[i]) {
+                dfs(isConnected, i, visited);
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public void dfs(int[][] isConnected, int n, boolean[] visited) {
+        visited[n]= true;
+        for(int j=0; j<isConnected.length; j++){
+            if(isConnected[n][j]==1 && !visited[j]){
+                dfs(isConnected, j, visited);
+            }
+        }
+    }
+}
