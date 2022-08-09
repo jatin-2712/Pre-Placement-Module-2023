7d2c286d6da5b049cc201ca42d3e45198d4dadbd
jatin-2712 <75720589+jatin-2712@users.noreply.github.com>
2022-08-09 14:21:54 +0530

    Create StackUsingQueue.java

---
+++ DS I/Day 9/StackUsingQueue.java
@@ -0,0 +1,45 @@
+class MyStack {
+
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+    private int top;
+
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+        q1.add(x);
+        top = x;
+    }
+
+    public int pop() {
+        while (q1.size() > 1) {
+            top = q1.remove();
+            q2.add(top);
+        }
+        int returnVal = q1.remove();
+        // swapping
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return returnVal;
+    }
+
+    public int top() {
+        return top;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
